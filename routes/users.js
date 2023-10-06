44132901d4c9020a89e5b825da36e86eda26204a
Alin Triff <triffalin@gmail.com>
2023-10-06 11:26:58 +0300

    parsed incoming file uploads with the multer package

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const multer = require('multer');
 
+const upload = multer({});
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('profiles');
+router.get('/', function (req, res) {
+	res.render('profiles');
 });
 
-router.get('/new-user', function(req, res) {
-  res.render('new-user');
+router.get('/new-user', function (req, res) {
+	res.render('new-user');
+});
+
+router.post('/profiles', upload.single('image'), function (req, res) {
+	const uploadedImageFile = req.file;
+	const userData = req.body;
 });
 
 module.exports = router;
