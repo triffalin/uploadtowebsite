c8597824b8ea3e43c12d4167b5bbc87a0a1a457b
Alin Triff <triffalin@gmail.com>
2023-10-06 15:38:34 +0300

    served uploaded files to website visitors

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use('/images', express.static('images'));
 
 app.use(userRoutes);
 
 db.connectToDatabase().then(function () {
-  app.listen(3000);
+	app.listen(3000);
 });
